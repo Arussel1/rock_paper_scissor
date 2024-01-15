eff48b78a95e0842913732b2cddcc5d1d2d96b87
n37le <anhminh05102004@gmail.com>
2024-01-14 23:04:58 -0500

    Fix colorto the right one

--- script.js
+++ script.js
@@ -17,12 +17,12 @@ function play(PlayerChoice){
     let ComputerChoice = ComputerChoices[Math.floor(Math.random() * 3)];
     if(PlayerChoice == ComputerChoice){
         document.getElementById("displaytext").innerText = ComputerChoice + " vs " + PlayerChoice + ". It's a tie";
-        document.getElementById("displaytext").style.color = "red";
     }else{
         if(ComputerWins[ComputerChoice] == PlayerChoice){
             document.getElementById("displaytext").innerText = ComputerChoice + " vs " + PlayerChoice +  ". Computer gets 1 point";
             ComputerScore++;
             document.getElementById("cscore").innerText = ComputerScore;
+            document.getElementById("displaytext").style.color = "red";
         }else{
             document.getElementById("displaytext").innerText = ComputerChoice + " vs " + PlayerChoice + ". Player gets 1 point";
             document.getElementById("displaytext").style.color = "green";
