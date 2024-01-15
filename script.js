18bd29ff672c8e3d06fce36282a2be78588cf7be
n37le <anhminh05102004@gmail.com>
2024-01-14 22:44:09 -0500

    Add text to clarify choices

--- script.js
+++ script.js
@@ -16,14 +16,14 @@ function play(PlayerChoice){
     }
     let ComputerChoice = ComputerChoices[Math.floor(Math.random() * 3)];
     if(PlayerChoice == ComputerChoice){
-        document.getElementById("displaytext").innerText = "It's a tie";
+        document.getElementById("displaytext").innerText = ComputerChoice + " vs " + PlayerChoice + ". It's a tie";
     }else{
         if(ComputerWins[ComputerChoice] == PlayerChoice){
-            document.getElementById("displaytext").innerText = "Computer gets 1 point";
+            document.getElementById("displaytext").innerText = ComputerChoice + " vs " + PlayerChoice +  ". Computer gets 1 point";
             ComputerScore++;
             document.getElementById("cscore").innerText = ComputerScore;
         }else{
-            document.getElementById("displaytext").innerText = "Player gets 1 point";
+            document.getElementById("displaytext").innerText = ComputerChoice + " vs " + PlayerChoice + ". Player gets 1 point";
             PlayerScore++;
             document.getElementById("pscore").innerText = PlayerScore;
         }
